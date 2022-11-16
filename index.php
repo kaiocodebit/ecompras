4d18a10d05dbd0b18eea35f7fcf6dce1424c2906
Kaio Felipe <kaio@codebit.com.br>
2022-11-16 12:24:20 -0300

    Fix classe SQL

--- index.php
+++ index.php
@@ -7,11 +7,13 @@ $app = new \Slim\Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	echo "OK";
+	$sql = new \kaiocodebit\DB\Sql();
+	$results = $sql->select("SELECT * FROM users");
+    // echo 'ok';
 
+	echo json_encode($results);
 });
 
 $app->run();
 
- ?>
+?>
